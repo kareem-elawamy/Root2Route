a777f7a83e6e2c8649f6f476ba58184bfcccee87
Fady Ashraf <alsyadf29@gmail.com>
2026-03-02 22:04:44 +0200

    feat: setup product module CQRS, endpoints, and update base repository

---
+++ BackEnd/Core/Features/Product/Command/Handlers/AddProductCommandHandler.cs
@@ -0,0 +1,50 @@
+﻿using Core.Features.Product.Command.Models;
+using Service.Services.ProductService;
+
+namespace Core.Features.Product.Command.Handlers
+{
+    public class AddProductCommandHandler : IRequestHandler<AddProductCommand, Response<string>>
+    {
+        private readonly IProductService _productService;
+
+        public AddProductCommandHandler(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public async Task<Response<string>> Handle(AddProductCommand request, CancellationToken cancellationToken)
+        {
+            if (!string.IsNullOrWhiteSpace(request.Barcode))
+            {
+                var isExist = await _productService.IsBarcodeExistAsync(request.Barcode);
+                if (isExist) return new Response<string>("هذا الباركود مسجل لمنتج آخر مسبقاً");
+            }
+
+            // Mapping
+            var newProduct = new Domain.Models.Product
+            {
+                OrganizationId = request.OrganizationId,
+
+                Name = request.Name,
+                Description = request.Description,
+                ImageUrl = request.ImageUrl, // (لو بتعمل Upload لصورة، هتهندل رفع الصورة هنا الأول وتحط اللينك)
+                StockQuantity = request.StockQuantity,
+
+                IsAvailableForDirectSale = request.IsAvailableForDirectSale,
+                DirectSalePrice = request.IsAvailableForDirectSale ? request.DirectSalePrice : 0,
+
+                IsAvailableForAuction = request.IsAvailableForAuction,
+                StartBiddingPrice = request.IsAvailableForAuction ? request.StartBiddingPrice : 0,
+
+                Barcode = request.Barcode,
+                ExpiryDate = request.ExpiryDate,
+                WeightUnit = request.WeightUnit,
+                ProductType = request.ProductType
+            };
+
+            await _productService.AddProductAsync(newProduct);
+
+            return new Response<string>("تمت إضافة المنتج بنجاح") { Succeeded = true };
+        }
+    }
+}
---
+++ BackEnd/Core/Features/Product/Command/Models/AddProductCommand.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Enums;
+
+namespace Core.Features.Product.Command.Models
+{
+    public class AddProductCommand : IRequest<Response<string>>
+    {
+        public Guid OrganizationId { get; set; }
+
+        public string Name { get; set; } = null!;
+        public string? Description { get; set; }
+        public int StockQuantity { get; set; }
+
+
+        public string? ImageUrl { get; set; }
+
+        public bool IsAvailableForDirectSale { get; set; }
+        public decimal DirectSalePrice { get; set; }
+
+        public bool IsAvailableForAuction { get; set; }
+        public decimal StartBiddingPrice { get; set; }
+
+        public string? Barcode { get; set; }
+        public DateTime? ExpiryDate { get; set; }
+        public WeightUnit? WeightUnit { get; set; }
+        public ProductType ProductType { get; set; }
+    }
+}
